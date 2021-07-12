7025757278dfbb1fd3241f9661792aaeb4e9e0f4
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-12 13:27:16 +0530

    feat(1.1c): Re-factor - Separate start-up logic and Fastify instantiation code

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+// Server-app, not the Server!
+import Fastify, { FastifyInstance } from 'fastify'
+import { USERS, RECIPES, RECIPE_CATEGORIES } from './fixtures'
+
+export const buildSever = (): FastifyInstance => {
+    // Instantiate the Fastify server
+    const fastify = Fastify()
+
+    // Register handlers to routes
+    fastify.get('/users', async () => USERS)
+    fastify.get('/recipes', async () => RECIPES)
+    fastify.get('/categories', async () => RECIPE_CATEGORIES)
+    fastify.get('*', async (req) => `You requested for ${req.url} using method ${req.method}, which does not have an associated response`)
+
+    // return the Server instance
+    return fastify
+}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
-import Fastify from 'fastify'
-import { USERS, RECIPES, RECIPE_CATEGORIES } from './fixtures'
+import { buildSever } from './app'
 
 const APP_PORT = process.env.APP_PORT || 3000
 
-const server = Fastify()
-
-server.get('/users', async () => USERS)
-
-server.get('/recipes', async () => RECIPES)
-
-server.get('/categories', async () => RECIPE_CATEGORIES)
-
-server.get('*', async (req) => `You requested for ${req.url} using method ${req.method}, which does not have an associated response`)
+const server = buildSever()
 
 server.listen(APP_PORT, () => console.log(`Server started on ${APP_PORT} 🚀`));
