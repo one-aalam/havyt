07e16e8288153462ff2be468bb1f8321376dbfab
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-12 12:45:46 +0530

    feat(1.1c): Use ES syntax and Async handlers

--- src/server.ts
+++ src/server.ts
@@ -5,20 +5,12 @@ const APP_PORT = process.env.APP_PORT || 3000
 
 const server = Fastify()
 
-server.get('/users', (req, res) => {
-    res.send(USERS)
-})
+server.get('/users', async () => USERS)
 
-server.get('/recipes', (req, res) => {
-    res.send(RECIPES)
-})
+server.get('/recipes', async () => RECIPES)
 
-server.get('/categories', (req, res) => {
-    res.send(RECIPE_CATEGORIES)
-})
+server.get('/categories', async () => RECIPE_CATEGORIES)
 
-server.get('*', (req, res) => {
-    res.send(`You requested for ${req.url} using method ${req.method}, which does not have an associated response`)
-})
+server.get('*', async (req) => `You requested for ${req.url} using method ${req.method}, which does not have an associated response`)
 
 server.listen(APP_PORT, () => console.log(`Server started on ${APP_PORT} 🚀`));
