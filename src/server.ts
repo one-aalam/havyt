5c6d6a2f8fa444b326988de334dd2283db06a103
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-12 11:26:02 +0530

    feat(1.1b): Respond to HTTP requests

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
 import http from 'http'
+import { USERS, RECIPES, RECIPE_CATEGORIES } from './fixtures'
 
-const server = http.createServer((req, res) => {
-  res.end(`You requested for ${req.url} using method ${req.method}`)
+const APP_PORT = process.env.APP_PORT || 3000
+
+const server = http.createServer((req: http.IncomingMessage, res: http.OutgoingMessage) => {
+  const route = req.method + ' ' + req.url // ex: GET /users
+
+  if (route === 'GET /users') {
+    res.end(JSON.stringify(USERS))
+  } else if (route === 'GET /recipes') {
+    res.end(JSON.stringify(RECIPES))
+  } else if (route === 'GET /categories') {
+    res.end(JSON.stringify(RECIPE_CATEGORIES))
+  } else {
+    res.end(
+      `You requested for ${req.url} using method ${req.method}, which does not have an associated response`
+    )
+  }
 })
 
-server.listen(process.env.APP_PORT, () => console.log(`Server started on ${process.env.APP_PORT} 🚀`))
+server.listen(APP_PORT, () => console.log(`Server started on ${APP_PORT} 🚀`))
