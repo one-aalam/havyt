a821513745a6a616f29bab04834ccfb922ba1ac9
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-12 11:24:21 +0530

    feat(1.1b): Add Types
    The data model for users, recipes and categories

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+export type User = {
+  id: number
+  firstName: string
+  lastName: string
+  email: string
+  avatar: string
+}
+
+export type Recipe = {
+  id: number
+  name: string
+  desc?: string
+  imageUrl?: string
+
+  courseId: number // we could've modelled this
+  cuisineId: number // and this better, but this designed to support the referential access model popular relatonal DB's use
+
+  serves: number
+  prepTime?: number
+  cookingTime: number
+
+  ingredients: string[]
+  directions: string[]
+
+  source?: string
+  tags?: string[]
+}
+
+export type RecipeCatgeory = {
+  id: number
+  type: string
+  name?: string
+  desc?: string
+}
+
+export type RecipeCatgeoryItem = {
+  id: number
+  categoryId: number
+  name?: string
+  desc?: string
+}
