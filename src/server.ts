7abbc253603cd3832035f34840651aa809c601ef
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-12 12:33:56 +0530

    feat(1.1c): Add + Integrate Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,24 @@
-import http from 'http'
+import Fastify from 'fastify'
 import { USERS, RECIPES, RECIPE_CATEGORIES } from './fixtures'
 
 const APP_PORT = process.env.APP_PORT || 3000
 
-const server = http.createServer((req: http.IncomingMessage, res: http.OutgoingMessage) => {
-  const route = req.method + ' ' + req.url // ex: GET /users
+const server = Fastify()
 
-  if (route === 'GET /users') {
-    res.end(JSON.stringify(USERS))
-  } else if (route === 'GET /recipes') {
-    res.end(JSON.stringify(RECIPES))
-  } else if (route === 'GET /categories') {
-    res.end(JSON.stringify(RECIPE_CATEGORIES))
-  } else {
-    res.end(
-      `You requested for ${req.url} using method ${req.method}, which does not have an associated response`
-    )
-  }
+server.get('/users', (req, res) => {
+    res.send(USERS)
 })
 
-server.listen(APP_PORT, () => console.log(`Server started on ${APP_PORT} 🚀`))
+server.get('/recipes', (req, res) => {
+    res.send(RECIPES)
+})
+
+server.get('/categories', (req, res) => {
+    res.send(RECIPE_CATEGORIES)
+})
+
+server.get('*', (req, res) => {
+    res.send(`You requested for ${req.url} using method ${req.method}, which does not have an associated response`)
+})
+
+server.listen(APP_PORT, () => console.log(`Server started on ${APP_PORT} 🚀`));
