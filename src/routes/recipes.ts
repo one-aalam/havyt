0d1a1eaadb76e9ab64a265b723ee159d24355f23
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-14 20:03:32 +0530

    feat(1.2c): Add POST, PUT, DELETE for recipes

--- src/routes/recipes.ts
+++ src/routes/recipes.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify'
+import { Recipe } from '../types'
 import { RECIPES } from '../fixtures'
 
 type RecipeParams = {
@@ -12,6 +13,12 @@ type RecipeQuerystring = {
     cuisineId?: number
     courseId?: number
 }
+
+// `id`s are created by us, so we'll omit it from the type
+type RecipeCreateBody = Omit<Recipe, 'id'>
+
+// mark all properties optional
+type RecipeUpdateBody = Partial<RecipeCreateBody>
 
 export default async function recipes(fastify: FastifyInstance) {
     // get all recipes having the provided `tag`, `cuisineId`, `categoryId`, or range, or all the recipes
@@ -44,5 +51,62 @@ export default async function recipes(fastify: FastifyInstance) {
             })
         }
         return recipe
+    })
+
+    fastify.post<{ Body: RecipeCreateBody }>('/recipes', async (req, reply) => {
+        const newRecipe: Recipe  = {
+            ...req.body,
+            id: RECIPES[RECIPES.length - 1].id + 1,
+        }
+        // RECIPES.push(newRecipe)
+        reply.code(201).send(newRecipe)
+    })
+
+    fastify.put<{
+        Params: RecipeParams,
+        Body: RecipeUpdateBody
+    }>('/recipes/:id', async (req, reply) => {
+
+        const recipe = RECIPES.find(recipe => recipe.id === parseInt(req.params.id))
+        if(!recipe) {
+            reply.code(404).send({
+                name: 'NotFoundError',
+                message: 'Not Found'
+            })
+        }
+
+        // get where is it
+        const recipeIndex = RECIPES.findIndex(recipe => recipe.id === parseInt(req.params.id))
+        // and update it
+        if(recipeIndex && recipe) {
+            RECIPES[recipeIndex] = {...recipe, ...req.body}
+            reply.code(200).send(recipe)
+        }
+
+        // It's not you, It's us!'
+        reply.code(500).send({
+            name: 'InternalServerError',
+            message: 'Internal Sever Error'
+        })
+    })
+
+
+    fastify.delete<{
+        Params: RecipeParams,
+    }>('/recipes/:id', async (req, reply) => {
+
+        // get where is it
+        const recipeIndex = RECIPES.findIndex(recipe => recipe.id === parseInt(req.params.id))
+
+        // and delete it
+        if(recipeIndex >= 0) {
+            RECIPES.splice(recipeIndex, 1)
+            reply.code(200).send({ deleted: true })
+        }
+
+        reply.code(404).send({
+            name: 'NotFoundError',
+            message: 'Not Found'
+        })
     })
 }
