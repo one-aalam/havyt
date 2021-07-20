c5cb2173f33668e89b943359057401da725eb611
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-20 13:29:38 +0530

    feat(1.4b): SSR with Pug

--- src/routes/web-app/index.ts
+++ src/routes/web-app/index.ts
@@ -2,7 +2,17 @@ import { FastifyInstance } from 'fastify'
 import path from 'path'
 
 export default async function users(fastify: FastifyInstance) {
+    fastify.register(import('point-of-view'), {
+        engine: {
+          pug: (await import('pug'))
+        },
+        root: path.join(__dirname, 'views'),
+        includeViewExtension: false,
+        defaultContext: {
+            dev: process.env.NODE_ENV !== 'production'
+        }
+    })
   // get all the users
-  fastify.get('/', async (req, reply) => reply.sendFile('index.html', path.join(__dirname, 'views')))
-  fastify.get('/auth', async (req, reply) => reply.sendFile('auth.html', path.join(__dirname, 'views')))
+  fastify.get('/', async (req, reply) => await reply.view('index'))
+  fastify.get('/auth', async (req, reply) => await reply.view('auth'))
 }
