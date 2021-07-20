5235e79a6c440050fda09aecd72f0770b616744e
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-20 12:11:35 +0530

    feat(1.4b): Fastify and SSR (with HTML files)

--- src/routes/web-app/index.ts
+++ src/routes/web-app/index.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import path from 'path'
 
 export default async function users(fastify: FastifyInstance) {
   // get all the users
-  fastify.get('/', async (req, reply) => reply.sendFile('index.html'))
-  fastify.get('/auth', async (req, reply) => reply.sendFile('auth.html'))
+  fastify.get('/', async (req, reply) => reply.sendFile('index.html', path.join(__dirname, 'views')))
+  fastify.get('/auth', async (req, reply) => reply.sendFile('auth.html', path.join(__dirname, 'views')))
 }
