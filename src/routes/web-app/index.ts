a5ad623bb392d418d356ecb8580e55e0942311a2
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-20 14:38:17 +0530

    feat(1.4b): it's all through the server now!

--- src/routes/web-app/index.ts
+++ src/routes/web-app/index.ts
@@ -1,7 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import path from 'path'
+import {
+    Recipe,
+    RecipeQuerystring,
+} from '../recipe/types'
 
-export default async function users(fastify: FastifyInstance) {
+export default async function routes(fastify: FastifyInstance) {
+    const recipeService = fastify.getStore<Recipe>('recipes')
+
     fastify.register(import('point-of-view'), {
         engine: {
           pug: (await import('pug'))
@@ -9,10 +15,28 @@ export default async function users(fastify: FastifyInstance) {
         root: path.join(__dirname, 'views'),
         includeViewExtension: false,
         defaultContext: {
-            dev: process.env.NODE_ENV !== 'production'
+            dev: process.env.NODE_ENV !== 'production',
+        },
+        options: {
+            basedir: path.join(__dirname, 'views')
         }
     })
-  // get all the users
-  fastify.get('/', async (req, reply) => await reply.view('index'))
+
+  fastify.get<{
+      Querystring: RecipeQuerystring
+  }>('/', async (req, reply) => {
+    const { offset = 0, limit = 10, tag, cuisineId, courseId } = req.query
+    const recipes = await recipeService.getAll()
+    const recipesTplData = recipes
+      .filter((recipe) => !tag || recipe.tags?.map((tag) => tag.toLowerCase()).includes(tag))
+      .filter((recipe) => !cuisineId || recipe.cuisineId == cuisineId)
+      .filter((recipe) => !courseId || recipe.courseId == courseId)
+      .slice(offset, offset + limit)
+
+    await reply.view('index', {
+        recipes: recipesTplData
+    })
+  })
+
   fastify.get('/auth', async (req, reply) => await reply.view('auth'))
 }
