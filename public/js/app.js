fd19e4d011d9adc600c0191882e37842ea01b7f3
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-22 19:04:11 +0530

    feat(1.4c): Enhance the layout

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
                     e.target.style.height = e.target.scrollHeight + 'px'
                 })
             }
+            if(elm.type === 'file') {
+                elm.addEventListener('change', (e) => {
+                    const imgEl = e.target.parentNode.querySelector('img')
+                    const fileNameEl = e.target.parentNode.querySelector('small')
+                    const file = e.target.files[0]
+                    imgEl.src = URL.createObjectURL(file)
+                    fileNameEl.innerHTML = file.name
+                })
+                elm.parentNode.querySelector('img').addEventListener('click', (e) => {
+                    e.target.parentNode.querySelector('input').click()
+                })
+            }
         })
     })
 })
