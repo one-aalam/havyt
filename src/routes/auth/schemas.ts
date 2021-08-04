3e6ee370f74a9fa6b325c550f0b484248bff89ec
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-08-04 22:44:37 +0530

    feat(2.1d): Issue tokens on Sign-in

--- src/routes/auth/schemas.ts
+++ src/routes/auth/schemas.ts
@@ -1,5 +1,5 @@
 import { errorSchema } from '../../lib/commons/schemas'
-import { userCreateSchema, userRespSchema } from '../user/schemas'
+import { userCreateSchema } from '../user/schemas'
 
 export const authSignInPayloadSchema = {
   $id: 'authSignInPayloadSchema',
@@ -12,10 +12,56 @@ export const authSignInPayloadSchema = {
   additionalProperties: false,
 } as const
 
+export const authTokenSchema = {
+    $id: 'authTokenSchema',
+    type: 'object',
+    properties: {
+        accessToken: { type: 'string' },
+        tokenType: { type: 'string' },
+        expiresIn: { type: 'number' },
+        expiresAt: { type: 'number' }
+    },
+    required: ['accessToken', 'tokenType', 'expiresIn', 'expiresAt' ],
+    additionalProperties: false,
+  } as const
+
+  export const authAppMetaDataSchema = {
+    $id: 'authAppMetaDataSchema',
+    type: 'object',
+    properties: {
+        provider: { type: 'string' },
+    },
+    required: ['provider' ],
+    additionalProperties: false,
+  } as const
+
+export const authSignUpRespSchema = {
+    $id: 'authSignUpRespSchema',
+    type: 'object',
+    properties: {
+        id: { type: 'string' },
+        email: { type: 'string' },
+        appMetadata: authAppMetaDataSchema
+    },
+    required: ['id', 'email', 'appMetadata' ],
+    additionalProperties: false,
+  } as const
+
+  export const authSignInRespSchema = {
+    $id: 'authSignInRespSchema',
+    type: 'object',
+    properties: {
+        token: authTokenSchema,
+        ...authSignUpRespSchema.properties
+    },
+    required: ['id', 'email', 'token', 'appMetadata' ],
+    additionalProperties: false,
+  } as const
+
 export const authSignInSchema = {
   body: authSignInPayloadSchema,
   response: {
-    '2xx': userRespSchema,
+    '2xx': authSignInRespSchema,
     '4xx': errorSchema,
   },
 } as const
@@ -23,7 +69,7 @@ export const authSignInSchema = {
 export const authSignUpSchema = {
     body: userCreateSchema,
     response: {
-      '2xx': userRespSchema,
+      '2xx': authSignUpRespSchema,
       '4xx': errorSchema,
     },
   } as const
--- src/routes/auth/service.ts
+++ src/routes/auth/service.ts
@@ -1,6 +1,9 @@
 import { Service } from 'typedi'
+import { createToken } from '../../lib/auth'
 import { UserService } from '../user/service'
+import { User } from '../user/types'
 import { AuthSignInPayload, AuthSignUpPayload } from './types'
+import envConfig from '../../config/env'
 
 @Service()
 export class AuthService {
@@ -9,7 +12,30 @@ export class AuthService {
     this.userService = userService
   }
 
-  signIn = async ({ email, password }: AuthSignInPayload) => await this.userService.getByEmailAndPassword(email, password)
+  signIn = async ({ email, password }: AuthSignInPayload) => {
+    const user = await this.userService.getByEmailAndPassword(email, password)
+    return this._genAuthResp(user)
+  }
 
-  signUp = async (body: AuthSignUpPayload) => await this.userService.create(body)
+  signUp = async (body: AuthSignUpPayload) => {
+    const user = await this.userService.create(body)
+    return this._genAuthResp(user, false)
+  }
+
+  // sendToken: As sign-in's are more frequent event than sign-ups
+  private _genAuthResp = (user: User, sendToken = true) =>  ({
+        id: user.id,
+        email: user.email,
+        token: sendToken ? this._genToken(user) : null,
+        appMetadata: {
+            provider: 'email'
+        }
+   })
+
+  private _genToken = ({ id, email }: User) =>  ({
+        accessToken: createToken({ id, email }),
+        tokenType: 'bearer',
+        expiresIn: envConfig.JWT_EXP,
+        expiresAt: new Date().getTime() + envConfig.JWT_EXP
+  })
 }
